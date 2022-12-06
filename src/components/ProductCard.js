55a0cc010fbdc026325daf1a3770c4aba9e6da77
AhshanHabib26 <ahshan.habib026@gmail.com>
2022-12-06 10:37:30 +0600

    Product Card Design

---
+++ src/components/ProductCard.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Styles from '../styles/ProductCard.module.css';
+import { BsFillCartCheckFill } from 'react-icons/bs';
+
+const ProductCard = ({ product }) => {
+  const { model, image, keyFeature, price, rating } = product;
+  return (
+    <div className="mt-3 px-4">
+      <div className={Styles.PRWrapper}>
+        <div className={Styles.PRContainer}>
+          <div className={Styles.imageContainer}>
+            <img src={image} alt="" />
+          </div>
+          <div className={Styles.detailsContainer}>
+            <div className={Styles.PRTitle}>
+              <h1>{model}</h1>
+            </div>
+            <div className={Styles.innerContainer}>
+              <div className={Styles.PRPrice}>
+                <h2>$ {price}</h2>
+              </div>
+              <div className={Styles.PRRating}>
+                <h2>{rating}</h2>
+              </div>
+            </div>
+            <div className={Styles.PRDetails}>
+              {keyFeature.map((key) => (
+                <li key={key}>{key}</li>
+              ))}
+            </div>
+            <div className={Styles.btnContainer}>
+              <div className={Styles.addToCart}>
+                <button>Add to Cart</button>
+              </div>
+              <div className={Styles.buyNow}>
+                <button>Buy Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ProductCard from '../components/ProductCard';
+import Styles from '../styles/Home.module.css'
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Hello</h1>
-        </div>
-    );
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch('products.json')
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <div className={Styles.HContainer}>
+      {products.map((product) => (
+        <ProductCard product={product} key={product._id} />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
